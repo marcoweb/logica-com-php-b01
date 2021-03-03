b22cbcef81acc9d9a6fa0c225b20ae682950769a
marcoweb <marcocarvalho.web@gmail.com>
2021-03-03 17:40:43 +0000

    Exemplo saque

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Olá Mundo!!!\n";
+// echo "Olá Mundo!!!\n";
+$saldo = 1000.00;
+$saque = 1000.00;
+
+if($saldo <= $saque) {
+    $saldo - $saque;
+    echo 'Sacado R$ ' . $saque . "\n";
+} else {
+    echo "Valor do saque maior que o saldo\n";
+}
